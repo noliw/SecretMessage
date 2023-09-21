f2c7f7e369ea8a926dfa378778989d1b99710bce
noliw <noliw2017@gmail.com>
2023-09-21 04:49:06 -0700

    message screen and welcome

---
+++ app/src/main/java/com/example/secretmessage/MessageFragment.kt
@@ -0,0 +1,8 @@
+package com.example.secretmessage
+
+import androidx.fragment.app.Fragment
+
+
+class MessageFragment : Fragment(R.layout.fragment_message) {
+
+}
---
+++ app/src/main/java/com/example/secretmessage/WelcomeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.secretmessage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val startButton = view.findViewById<Button>(R.id.start_btn)
+        startButton.setOnClickListener {
+            findNavController().navigate(R.id.action_welcomeFragment_to_messageFragment)
+        }
+    }
+}
